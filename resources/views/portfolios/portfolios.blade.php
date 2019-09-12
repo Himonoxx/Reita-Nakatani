86ba84dda09c7f80a9226c64a03eacd14df6e76c
reita.nakatani <himono55@gmail.com>
2019-09-12 11:09:25 +0000

    portfolio_fix

--- resources/views/portfolios/portfolios.blade.php
+++ resources/views/portfolios/portfolios.blade.php
@@ -5,73 +5,32 @@
     <h1 class="midashi"><b>Portfolios</b></h1>
   </div>
         <div class="card col-sm-10 offset-sm-1 mt-0">
-            <div class="card-deck mt-5 mb-5">
-               <?php $cnt=1; ?>
-               @if(count($portfolios)<=0)
-                 <h1 class="text-center mx-auto">Nothing Portfolios</h1>
-                @endif
-              @foreach($portfolios as $portfolio)
-          
-                @if($cnt % 4 == 0)
-                    <div class="card bg-secondary text-light">
-                      @if($portfolio->image == null)
-                        <img class="card-img-top" src="/images/dummy.png" alt="Card image cap">
-                      @else
-                        <img class="card-img-top" src={{ $portfolio->image }} onerror="this.src='/images/dummy.png';" alt="Card image cap">
-                      @endif
-                      <div class="card-body">
-                        <h4 class="card-title">{{ $portfolio->title }}</h4>
-                        <p class="card-text">{{ $portfolio->comment }}</p>
-                        <p class="card-text"><small class="text-light mb-auto">date:{{ \Carbon\Carbon::createFromTimestamp($portfolio->timestamps)->format("m/d/Y") }}</small></p>
-                        <div class="mb-auto">
-                          <a class="btn btn-light text-dark btn-block" href={{ $portfolio->siteurl }} role="button">Visit Website</a>
-                          @if(Auth::check())
-                            @if(Auth::user()->id == $portfolio->user_id)
-                              {!! link_to_route('portfolios.edit','Edit',['id'=>$portfolio->id],['class'=>'btn btn-info text-dark btn-block']) !!}
-                              {!! link_to_route('portfolios.upload','Capture Upload',['id'=>$portfolio->id],['class'=>'btn btn-info text-dark btn-block']) !!}
-                            @endif
-                          @endif
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="card-deck mt-5 mb-5">
-                @else
-                
-                  <div class="card bg-secondary text-light">
-                    @if($portfolio->image == null)
-                      <img class="card-img-top" src="/images/dummy.png" alt="Card image cap">
-                    @else
-                      <img class="card-img-top" src={{ $portfolio->image }} onerror="this.src='/images/dummy.png';" alt="Card image cap">
+          <div class="d-flex" style="flex-wrap: wrap;">
+          @foreach($portfolios as $portfolio)
+            <div class="card mt-3 bg-secondary text-light m-3" style="width:23%;">
+              @if ($portfolio->image == null)
+                <img class="card-img-top" src="/images/dummy.png" alt="images cap">
+              @else
+                <img class="card-img-top" src={{ $portfolio->image }} onerror="this.src='/images/dummy.png';" alt="images cap">
+              @endif
+              <div class="card-body">
+                <h5 class="card-title">{{ $portfolio->title }}</h5>
+                <p class="card-text">{{ $portfolio->comment }}</p>
+                <p class="card-text"><small class="text-light mb-auto">date:{{ \Carbon\Carbon::createFromTimestamp($portfolio->timestamps)->format("m/d/Y") }}</small></p>
+                <div class="mb-auto">
+                  <a class="btn btn-light text-dark btn-block" href={{ $portfolio->siteurl }} role="button">Visit Website</a>
+                  @if(Auth::check())
+                    @if(Auth::user()->id == $portfolio->user_id)
+                      {!! link_to_route('portfolios.edit','Edit',['id'=>$portfolio->id],['class'=>'btn btn-info text-dark btn-block']) !!}
+                      {!! link_to_route('portfolios.upload','Capture Upload',['id'=>$portfolio->id],['class'=>'btn btn-info text-dark btn-block']) !!}
                     @endif
-                    <div class="card-body">
-                      <h4 class="card-title">{{ $portfolio->title }}</h4>
-                      <p class="card-text">{{ $portfolio->comment }}</p>
-                      <p class="card-text"><small class="text-light mb-auto">{{ date_format($portfolio->created_at,'Y-m-d') }}</small></p>
-                      <div class="mb-auto">
-                        <a class="btn btn-light text-dark btn-block" href={{ $portfolio->siteurl }} role="button">Visit Website</a>
-                        @if(Auth::check())
-                          @if(Auth::user()->id == $portfolio->user_id)
-                            {!! link_to_route('portfolios.edit','Edit',['id'=>$portfolio->id],['class'=>'btn btn-info text-dark btn-block']) !!}
-                            {!! link_to_route('portfolios.upload','Capture Upload',['id'=>$portfolio->id],['class'=>'btn btn-info text-dark btn-block']) !!}
-                          @endif
-                        @endif
-                    </div>
-                    </div>
-                  </div>
-                
-
-                  
-                
-                @endif
-                <?php $cnt=$cnt+1; ?>
-                @endforeach
+                  @endif
                 </div>
-              
-              
-    
-    
-</div>
+              </div>
+            </div>
+          @endforeach
+          </div>
+        </div>
 
 
 
